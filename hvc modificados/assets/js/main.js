77912cedad4a9eafd875def2dbb207ca15f3e99c
Your Name <you@example.com>
2020-02-24 20:16:12 -0500

    Cambios en el html

--- hvc modificados/assets/js/main.js
+++ hvc modificados/assets/js/main.js
@@ -1,3 +1,19 @@
-document.querySelector('html').onclick = function(){
-	alert('Ouch! Deja de pincharme');
+
+
+window.onload = function(){
+	
+    var miSrc = document.getElementById('fotito');
+	alert(miSrc);
+	
 }
+
+// var miImage = document.querySelector('fotito');
+
+// miImage.onclick = function(){
+	// var miSrc = miImage.getElementsByClassName('fotito');
+	// if(miSrc === '/assets/images/foto.jpg'){
+		// miImage.setAttribute('src','/assets/images/foto2.jpg');
+	// }else{
+		// miImage.setAttribute('src','/assets/images/foto.jpg');
+	// }
+// }
